edd628e0890a5503cccf0bbcb874a63ee482dcb1
Maxim <kalinkin.fiz@gmail.com>
2021-10-21 21:08:17 +0300

    Initial commit

---
+++ src/cart/cart.controller.ts
@@ -0,0 +1,92 @@
+import { Controller, Get, Delete, Put, Body, Req, Post, UseGuards, HttpStatus } from '@nestjs/common';
+
+// import { BasicAuthGuard, JwtAuthGuard } from '../auth';
+import { OrderService } from '../order';
+import { AppRequest, getUserIdFromRequest } from '../shared';
+
+import { calculateCartTotal } from './models-rules';
+import { CartService } from './services';
+
+@Controller('api/profile/cart')
+export class CartController {
+  constructor(
+    private cartService: CartService,
+    private orderService: OrderService
+  ) { }
+
+  // @UseGuards(JwtAuthGuard)
+  // @UseGuards(BasicAuthGuard)
+  @Get()
+  findUserCart(@Req() req: AppRequest) {
+    const cart = this.cartService.findOrCreateByUserId(getUserIdFromRequest(req));
+
+    return {
+      statusCode: HttpStatus.OK,
+      message: 'OK',
+      data: { cart, total: calculateCartTotal(cart) },
+    }
+  }
+
+  // @UseGuards(JwtAuthGuard)
+  // @UseGuards(BasicAuthGuard)
+  @Put()
+  updateUserCart(@Req() req: AppRequest, @Body() body) { // TODO: validate body payload...
+    const cart = this.cartService.updateByUserId(getUserIdFromRequest(req), body)
+
+    return {
+      statusCode: HttpStatus.OK,
+      message: 'OK',
+      data: {
+        cart,
+        total: calculateCartTotal(cart),
+      }
+    }
+  }
+
+  // @UseGuards(JwtAuthGuard)
+  // @UseGuards(BasicAuthGuard)
+  @Delete()
+  clearUserCart(@Req() req: AppRequest) {
+    this.cartService.removeByUserId(getUserIdFromRequest(req));
+
+    return {
+      statusCode: HttpStatus.OK,
+      message: 'OK',
+    }
+  }
+
+  // @UseGuards(JwtAuthGuard)
+  // @UseGuards(BasicAuthGuard)
+  @Post('checkout')
+  checkout(@Req() req: AppRequest, @Body() body) {
+    const userId = getUserIdFromRequest(req);
+    const cart = this.cartService.findByUserId(userId);
+
+    if (!(cart && cart.items.length)) {
+      const statusCode = HttpStatus.BAD_REQUEST;
+      req.statusCode = statusCode
+
+      return {
+        statusCode,
+        message: 'Cart is empty',
+      }
+    }
+
+    const { id: cartId, items } = cart;
+    const total = calculateCartTotal(cart);
+    const order = this.orderService.create({
+      ...body, // TODO: validate and pick only necessary data
+      userId,
+      cartId,
+      items,
+      total,
+    });
+    this.cartService.removeByUserId(userId);
+
+    return {
+      statusCode: HttpStatus.OK,
+      message: 'OK',
+      data: { order }
+    }
+  }
+}
